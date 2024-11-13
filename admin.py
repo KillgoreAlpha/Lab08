0bb3a65eb417cfcf268b0365c104c0a0185e2025
Alex Villa <aevilla160@gmail.com>
2024-11-12 19:16:17 -0800

    added student view and admin

--- admin.py
+++ admin.py
@@ -1,6 +1,20 @@
 from flask import Flask
 from flask_admin import admin
+from flask_admin.contrib.sqla import ModelView
+from backend import db
+from backend import *
 
 app = Flask(__name__)
 
 app.run
+
+admin = admin.Admin(app, name='Admin', template_mode='bootstrap3')
+
+class studentView(ModelView):
+    column_list = ['name', 'email']
+    can_create = False
+    can_edit = False
+    can_delete = False
+
+admin.add_view(studentView(Student, db.session))
+
