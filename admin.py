20ea895a42d783016b186a009dd80f52644a7c84
Armando Sanchez <127352361+armjsan@users.noreply.github.com>
2024-11-12 19:18:26 -0800

    Adding teacher and course view in admin page

--- admin.py
+++ admin.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from flask_admin import admin
 from flask_admin.contrib.sqla import ModelView
-from backend import db
+from flask_admin import admin
 from backend import *
+from backend import db
 
 app = Flask(__name__)
 
@@ -18,3 +18,25 @@ class studentView(ModelView):
 
 admin.add_view(studentView(Student, db.session))
 
+
+admin = admin.Admin(app, name="Admin", templaye_mode = 'bootstrap3')
+
+
+
+
+
+
+class teacherView(ModelView):
+    can_edit = True
+    can_delete = False
+    can_create = False
+    column_searchable_list = ['name', 'grade']
+
+class courseView(ModelView):
+    can_edit = False
+    can_delete = False
+    can_delete = False
+    
+
+
+admin.add_view(teacherView(Instructor, db.session))
