81b525d64b4cebb5ff178050f25e6a643acad643
Armando Sanchez <127352361+armjsan@users.noreply.github.com>
2024-11-18 14:18:05 -0800

    Admin and Login WIP

--- admin.py
+++ admin.py
@@ -1,30 +1,21 @@
 from flask import Flask
 from flask_admin.contrib.sqla import ModelView
-from flask_admin import admin
-from backend import *
-from backend import db
+from flask_admin import Admin
+# from backend import *
+from models import *
+from db_config import db
 
 app = Flask(__name__)
 
-app.run
+# app.run
 
-admin = admin.Admin(app, name='Admin', template_mode='bootstrap3')
+admin = Admin.Admin(app, name='Admin', template_mode='bootstrap3')
 
 class studentView(ModelView):
     column_list = ['name', 'email']
     can_create = False
     can_edit = False
     can_delete = False
-
-admin.add_view(studentView(Student, db.session))
-
-
-# admin = admin.Admin(app, name="Admin", templaye_mode = 'bootstrap3')
-
-
-
-
-
 
 class teacherView(ModelView):
     can_edit = True
@@ -35,9 +26,13 @@ class teacherView(ModelView):
 class courseView(ModelView):
     can_edit = False
     can_delete = False
-    can_delete = False
+    can_create = False
     
 
 
 admin.add_view(teacherView(Instructor, db.session))
 admin.add_view(courseView(Course,db.session))
+admin.add_view(studentView(Student, db.session))
+
+if __name__ == "__main__":
+    app.run(debug=True)
