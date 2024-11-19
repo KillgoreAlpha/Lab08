b2b87ae6f7d31e665a66fbd789eac482cf390d29
Armando Sanchez <127352361+armjsan@users.noreply.github.com>
2024-11-18 21:15:33 -0800

    Logic for Instructor and Student regarding grades and courses

--- login_instruct.py
+++ login_instruct.py
@@ -2,7 +2,7 @@ from flask import Blueprint,request, jsonify
 from flask_login import  login_user, logout_user, current_user,login_required
 from werkzeug.security import check_password_hash
 from db_config import db
-from models import Instructor
+from models import Instructor, Course, Student
 
 instruct_login_bp = Blueprint('instruct_login',__name__)
 
@@ -26,3 +26,55 @@ def logout():
         logout_user()
         return jsonify({"message":"Instructor Logged Out"})
     return jsonify({"error":"Only Instructor can log out here"}), 403
+
+@instruct_login_bp.route('/my-classes',methods=['GET'])
+@login_required
+def view_courses():
+    if current_user.role != 'instruct':
+        return jsonify({"error":"Unauthorized"}),403
+    classes = Course.query.filter_by(instructor=current_user.name).all()
+    return jsonify({"classes": [course.to_dict() for course in classes]})
+
+@instruct_login_bp.route('/class/<int:class_id>/students',methods=['GET'])
+@login_required
+def view_students_in_class(class_id):
+    if current_user.role != 'instruct':
+        return jsonify({"error":"Unauthorized"}),403
+    
+    course = Course.query.get(class_id)
+    if not course or course.instructor != current_user.name:
+        return jsonify({"error":"Class not found or authourized to view"}),404
+
+    return jsonify({
+        "courses":course.name,
+        "students":[student.to_dict() for student in course.students]
+    })
+
+#Edit Grade
+@instruct_login_bp.route('/class/<int:class_id>/student/<int:student_id>/grade', methods=['PUT'])
+@login_required
+
+def edit_grade(class_id,student_id):
+    if current_user.role != 'instruct':
+        return jsonify({"error":"Unauthorized"}),403
+    
+    course = course.query.get(class_id)
+    if not course or course.instructor != current_user.name:
+        return jsonify({"error":"Class not found or not authorized to view"}), 404
+    
+    student = Student.query.get(class_id)
+    if not student or student != course.students:
+        return jsonify({"error":"Student not found or not authorized to view"}), 404
+
+    data = request.get_json()
+    new_grade = data.get('grade')
+
+    if not new_grade:
+        return jsonify({"error": "Invalid data"}), 4004
+    try:
+        student.grade = float(new_grade)
+        db.session.commit()
+        return jsonify({"message":f"Grade updated for {student.name} in {course.name}"})
+    except ValueError:
+        return jsonify({"error": "Invalid grade format"}), 400
+    
--- login_student.py
+++ login_student.py
@@ -2,7 +2,7 @@ from flask import Blueprint,request, jsonify
 from flask_login import  login_user, logout_user, current_user,login_required
 from werkzeug.security import check_password_hash
 from db_config import db
-from models import Student
+from models import Student, Course
 
 student_login_bp = Blueprint('student_login',__name__)
 
@@ -26,3 +26,32 @@ def logout():
         logout_user()
         return jsonify({"message":"Student Logged Out"})
     return jsonify({"error":"Only students can log out here"}), 403
+
+@student_login_bp.route('enroll',methods=['POST'])
+@login_required
+def enroll_course():
+    data = request.get_json()
+    course_id = data.get('course_id')
+
+    student = current_user
+    course = Course.query.get(course_id)
+
+    if not course:
+        return jsonify({"error":"Course not found"}, 404)
+    if student in course.students:
+        return jsonify({"message":"Already enrolled"}, 400)
+    if len(course.students) >= course.capacity:
+        return jsonify({"message":"Course is full"}, 404)
+    course.students.append(student)
+    db.session.commit()
+    return jsonify({"message":f"{student.name} enrolled in {course.name}"})
+
+#view enrolled courses
+@student_login_bp.route('/my-courses',methods=['GET'])
+@login_required
+def get_courses():
+    student = current_user
+    return jsonify({"courses":[course.to_dict() for course in student.courses]})
+
+
+ 
